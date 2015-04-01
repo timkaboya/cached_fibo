f4f4b94cc7660ededccce1f2941c61804774ef84
Timmay <timothy.kaboya@gmail.com>
2015-04-01 18:05:13 -0500

    The normal Fibo algorithm
    The normal fibo algo just for comparison.
    
    Simple helpful programs, I like. :)

---
+++ fibo.py
@@ -0,0 +1,8 @@
+"""
+    This program generates the requested fibonacci number. 
+    Note that this program will run for long if the requested fib number is quite high.
+    Ex: 300th Fibonacci number
+    This program does not utilize any memory optimization techniques and serves to demonstrate that in tandem with the other program
+"""
+
+
