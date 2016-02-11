84c18fd3f9b167c4cf9c21194f22fe1944fab303
Timmay <timothy.kaboya@gmail.com>
2016-02-11 18:08:59 -0500

    Fixed errors in Program
    Recursive fibo now gives correct numbers.

--- fibo.py
+++ fibo.py
@@ -2,26 +2,26 @@
     This program generates the requested fibonacci number. 
     Note that this program will run for long if the requested fib number is quite high.
     Ex: 300th Fibonacci number
-    This program does not utilize any memory optimization techniques and serves to demonstrate that in tandem with the other program
+    This program does not utilize any memory optimization techniques and serves to demonstrate memoization in tandem with the other program
 """
 
 
 def fibo(n):
-	if n == 0 or n == 1:
-		return n
-	else:
-        for i in range(n):
-			n = i + n
-		return n
-     
-            
+    assert n > -1
+
+    if n == 0 or n == 1:
+        return n
+    else:
+        return fibo(n-1) + fibo(n-2)
+
+
 #TODO
 # Recursive Algorithm for later. 
 
 def main(): 
-	while True:
-		print (fibo(int(input('Which fibonacci number do you want: '))))
-		
+    while True:
+        print (fibo(int(input('Which fibonacci number do you want: '))))
+
 
 if __name__ == '__main__':
-	main()
+    main()
